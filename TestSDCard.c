5b8d10662062054281d6050de77809ecd85e3c51
Kyle Rabbitt <kerabbitt@wpi.edu>
2024-11-10 11:31:53 -0500

    Init

---
+++ TestSDCard.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hw_config.h"
+#include "f_util.h"
+#include "ff.h"
+//#include "ffconf.h"
+//#include "sd_card.h"
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+// #define SPI_PORT spi0
+// #define PIN_MISO 16
+// #define PIN_CS   17
+// #define PIN_SCK  18
+// #define PIN_MOSI 19
+
+int main()
+{
+    stdio_init_all();
+
+    sd_init_driver();
+    // SPI initialisation. This example will use SPI at 1MHz.
+    // spi_init(SPI_PORT, 31250000);
+    // gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    // gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
+    // gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+    // gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    
+    // // Chip select is active-low, so we'll initialise it to a driven-high state
+    // gpio_set_dir(PIN_CS, GPIO_OUT);
+    // gpio_put(PIN_CS, 1);
+    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+
+    // See FatFs - Generic FAT Filesystem Module, "Application Interface",
+    // http://elm-chan.org/fsw/ff/00index_e.html
+    FATFS fs;
+    FRESULT fr = f_mount(&fs, "0:", 1);
+    if (FR_OK != fr) {
+        panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+
+    // Open a file and write to it
+    FIL fil;
+    const char* const filename = "test.csv";
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if (FR_OK != fr && FR_EXIST != fr) {
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    }
+    if (f_printf(&fil, "Hello, world - 2!\nNew row?, yay") < 0) {
+        printf("f_printf failed\n");
+    }
+
+    // Close the file
+    fr = f_close(&fil);
+    if (FR_OK != fr) {
+        printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+
+    // Unmount the SD card
+    f_unmount("");
+
+    puts("Goodbye, world!");
+
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
